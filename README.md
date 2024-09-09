9d49394a0fabb0a59f3084920b6a65dbecacb48a
Ariane Mora <ariane.n.mora@gmail.com>
2024-09-09 14:57:06 -0700

    Updated files

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Docko
-
 Docking for ligands made simple.
 
 ## Install
-
 Make sure you have vina installed. 
 
 ```
@@ -20,4 +18,29 @@ pip install -r requirements.txt
 ## PDB or structure
 You need to select your structure from PDB or in liu of that, use alphafold3 server (https://alphafoldserver.com/).
 
-If you use the alphafoldserver you'll get 
+If you use the alphafoldserver you'll get `cif` files and this works with that too!
+
+## Working with heme based files
+Unfortunatley since alphafold is some new stuff and we're working with autodock vina we will need to change the files a bit. 
+
+First, if we use the AF3 docked heme, this will be automatically "cleaned" before making the pdbqt file. So we use the pipeline on the AF3 structure.
+
+So we need to read-add it back in after also converting it manually. 
+
+To convert it manually, we copy and paste (i know lol) the heme from the original pdb file (if you don't have this, go into a program like chimeraX and convert the .cif file to a .pdb file).
+
+Then you open up the alpha fold pdb in a text editor and copy out the heme atoms, ommitting the last one, the `Fe`, as vina doesn't like this one.
+
+Then, we convert this manually by using obabel: ` obabel heme.pdb -o pdbqt > heme.pdbqt` 
+
+Once this has been converted, we realise that vina doesn't like many of the tags. So we need to then change this so that we remove all of these.
+These include (but probably not limited to)
+
+```
+ENDBRANCH
+ROOT
+BRANCH
+ENDROOT
+```
+
+Then you can run the program as per usual :D 
--- data/hayden/dock.py
+++ data/hayden/dock.py
@@ -7,21 +7,21 @@ sys.path.append('/Users/arianemora/Documents/code/docko/docko/')
 from docko import *
 from pathlib import Path
 
-smiles = 'CC1=C(C2=CC3=NC(=CC4=C(C(=C([N-]4)C=C5C(=C(C(=N5)C=C1[N-]2)C=C)C)C=C)C)C(=C3CCC(=O)[O-])C)CCC(=O)[O-].[Fe]' # heme
+# smiles = 'CC1=C(C2=CC3=NC(=CC4=C(C(=C([N-]4)C=C5C(=C(C(=N5)C=C1[N-]2)C=C)C)C=C)C)C(=C3CCC(=O)[O-])C)CCC(=O)[O-].[Fe]' # heme
 smiles = 'O=[P@@]([H])(C)C1=CC=CC=C1' # ent1
 smiles = 'O=[P@]([H])(C)C1=CC=CC=C1' # ent2
 
 # , 270, 240
 """ Test cleaning a pdb structure so that we can have """
 base_dir = '/Users/arianemora/Documents/code/docko/data/hayden/'
-label = 'fold_2024_08_28_hayden'
+label = 'fold_2024_08_28_hayden_with_heme'
 
-#clean_one_pdb(f'{base_dir}fold_casey_04092024_model_0.cif', f'{base_dir}{label}/{label}.pdb')
+#clean_one_pdb(f'{base_dir}{label}/fold_2024_08_28_hayden_with_heme.pdb', f'{base_dir}{label}/{label}.pdb')
 
 # """ Test converting a pdb to a pdbqt file. """
 #pdb_to_pdbqt_protein(f'{base_dir}{label}/{label}.pdb', f'{base_dir}{label}/{label}.pdbqt')
 # Had to manually go obabel fold_2024_08_28_hayden.pdb -o pdbqt -xr -p 7.4 > fold_2024_08_28_hayden.pdbqt
-score = dock(sequence='', protein_name=label, smiles=smiles, ligand_name='ent2', residues=[100], 
+score = dock(sequence='', protein_name=label, smiles=smiles, ligand_name='ent1', residues=[89], 
             protein_dir=f'{base_dir}', ligand_dir=f'{base_dir}', output_dir=f'{base_dir}vina_output/', pH=7.4,
-            method='vina', size_x=5.0, size_y=5.0, size_z=5.0)
+            method='vina', size_x=10.0, size_y=10.0, size_z=10.0)
 print(score)
